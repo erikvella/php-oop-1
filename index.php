ef8be51144c0606b8e84dab91a7408e9d79acd89
erikvella <vella.erik1@gmail.com>
2023-11-06 18:33:06 +0100

    [add] bonus 2

--- index.php
+++ index.php
@@ -27,10 +27,10 @@ require_once __DIR__ . '/db/db.php';
   <div class="container d-flex">
 <?php foreach($Movies as $Movie): ?>
     <div class="card">
-      <img src="" alt="">
-      <h3>Titolo film  Genere : </h3>
-      <h4>Durata : </h4>
-      <p>Cast</p>
+      <img src="img/<?php echo $Movie->image->imgName ?>" alt="<?php echo $Movie->image->imgName ?>">
+      <h3>Titolo: <?php echo $Movie->title ?> <br> Genere: <?php echo $Movie->genere ?></h3>
+      <h4>Durata : <?php echo $Movie->durata ?></h4>
+      <p><strong>Cast: </strong><?php echo implode(', ' , $Movie->attori) ?></p>
       
     </div>
 <?php endforeach; ?>
